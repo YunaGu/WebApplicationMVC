2847b6395646f6c0ce3fcf49a097546dac3044b8
Yun Gu <guy36@cardiff.ac.uk>
2023-12-08 10:07:12 +0000

    Add MsgId in UserInputModel

--- WebApplication1/Models/UserInputModel.cs
+++ WebApplication1/Models/UserInputModel.cs
@@ -1,16 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApplication1.Models;
 
 public class UserInputModel
 {
+    [Key]
+    public int MsgId
+    {
+        get;
+        set;
+    }
+    
+    [Required]
     public string UserInput
     {
         get;
         set;
     }
 
-    public DateTime InputTime
+    public DateTime InputTime 
     {
         get;
         set;
-    }
+    } = DateTime.Now;
 }
